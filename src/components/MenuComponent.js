d5f171fdeb51828c10bda601c126e72ba2b366ec
gmoraitis <moraitis.georgios@icloud.com>
2020-07-31 17:29:03 +0300

    First Assingment

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,6 +4,7 @@ import {
   Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle
 } from 'reactstrap';
+import Dishdetail from './DishDetailComponent';
 
 class Menu extends Component {
 
@@ -19,22 +20,6 @@ class Menu extends Component {
     this.setState({ selectedDish: dish });
   }
 
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else
-      return (
-        <div></div>
-      );
-  }
 
   render() {
     const menu = this.props.dishes.map((dish) => {
@@ -56,10 +41,8 @@ class Menu extends Component {
         <div className="row">
           {menu}
         </div>
+        <Dishdetail dish={this.state.selectedDish} />
         <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
-          </div>
         </div>
       </div>
     );
